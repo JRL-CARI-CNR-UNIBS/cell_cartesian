abc939bbef7e6b6de0fcf9d53cfb3baa1f82a010
CesareTonola <c.tonola001@unibs.it>
2024-06-28 10:57:03 +0200

    ompl pipeline

--- cartesian_moveit_config/launch/demo.launch.py
+++ cartesian_moveit_config/launch/demo.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("cartesian_cell", package_name="cartesian_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("cartesian_cell", package_name="cartesian_moveit_config").planning_pipelines(pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]).to_moveit_configs()
     return generate_demo_launch(moveit_config)
